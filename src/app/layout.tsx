8675152912c14c371be7dc3762234bb6bbfee94e
Jason Bert <jason.bert@scorado.com>
2023-12-10 14:28:53 +0000

    Add SASS compiler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "@fontsource/open-sans"
+import "@fontsource/outfit"
 
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from 'next'
+import baseStyles from '~f/framework/base.module.scss'
+import './global.scss'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <h1 className={baseStyles.Heading__highlighted}>Cosmos Example Application</h1>
+        {children}
+      </body>
     </html>
   )
 }
