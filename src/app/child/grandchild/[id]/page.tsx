a2894a8491cbd7be2f99f49def265294c9f5ff5a
Jason Bert <jason.bert@scorado.com>
2023-12-17 21:06:38 +0000

    Change grandchild pages to dynamic

--- src/app/child/grandchild/[id]/page.tsx
+++ src/app/child/grandchild/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { Heading1, Heading2 } from '~f/framework/heading';
 import FullContainer from '~f/framework/layout/container';
 
-export default function Page() {
+interface PageProps {
+  params: {
+    id: string;
+  }
+}
+
+export default function Page({ params }: PageProps) {
   return (
     <FullContainer>
-      <Heading1>Grandchild 1 Page</Heading1>
+      <Heading1>Grandchild {params.id} Page</Heading1>
       <Heading2>An example of nesting layouts</Heading2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
