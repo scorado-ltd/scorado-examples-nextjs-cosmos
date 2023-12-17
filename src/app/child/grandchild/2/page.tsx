2367fc302cb4a0654b9e3339b2de141992173654
Jason Bert <jason.bert@scorado.com>
2023-12-17 15:24:30 +0000

    Furtther examples of nesting layouts

--- src/app/child/grandchild/2/page.tsx
+++ src/app/child/grandchild/2/page.tsx
@@ -1,17 +1,11 @@
-import Link from 'next/link';
 import { Heading1, Heading2 } from '~f/framework/heading';
 import FullContainer from '~f/framework/layout/container';
 
 export default function Page() {
   return (
     <FullContainer>
-      <Heading1>Child Page</Heading1>
+      <Heading1>Grandchild 2 Page</Heading1>
       <Heading2>An example of nesting layouts</Heading2>
-      <br />
-      <ul>
-        <li><Link href={'/child/grandchild'}>Grandchild Section</Link></li>
-      </ul>
-      <br />
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
---
+++ src/app/child/grandchild/layout.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next"
+import Link from "next/link"
+import { PropsWithChildren } from "react"
+import { Heading1, Heading2 } from "~f/framework/heading"
+import FullContainer from "~f/framework/layout/container"
+
+export const metadata: Metadata = {
+    title: 'Child Section',
+    description: 'Child section description',
+}
+
+export default function RootLayout({ children }: PropsWithChildren) {
+    return (
+        <>
+            <FullContainer>
+                <Heading1>Grandchild Page</Heading1>
+                <Heading2>An example of nesting layouts</Heading2>
+                <br />
+                <ul>
+                    <li><Link href={'/child/grandchild/1'}>Grandchild 1</Link></li>
+                    <li><Link href={'/child/grandchild/2'}>Grandchild 2</Link></li>
+                </ul>
+                <br />
+            </FullContainer>
+            {children}
+        </>
+    )
+}
