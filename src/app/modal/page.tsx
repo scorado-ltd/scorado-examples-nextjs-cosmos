d7ff27539daddbeb3e3dfd594ae99731eecea6f5
Jason Bert <jason.bert@scorado.com>
2023-12-30 16:41:43 +0000

    Implement close functionality and router modal

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -1,10 +1,14 @@
+import { PrimaryButton } from "~f/framework/button";
 import { Heading1 } from "~f/framework/heading";
 import Modal from "~f/framework/modal";
 
 export default function Page() {
     return (
         <div>
-            <Modal header="Test Modal">
+            <Modal
+                header="Test Modal"
+                footer={<PrimaryButton>Submit</PrimaryButton>}
+            >
                 <p>Modal content 1</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
--- src/feature/framework/modal/index.tsx
+++ src/feature/framework/modal/index.tsx
@@ -1,14 +1,40 @@
-import { PropsWithChildren } from 'react'
-import { Heading2 } from '../heading'
-import styles from './index.module.scss'
+'use client'
+
+import { useRouter } from "next/navigation";
+import React, { MouseEventHandler, PropsWithChildren, useCallback, useEffect, useRef } from 'react';
+import { Heading2 } from '../heading';
+import styles from './index.module.scss';
 
 interface ModalProps extends PropsWithChildren {
     header?: string | React.ReactNode
+    footer?: React.ReactNode
+    onClose?: () => void
 }
 
-export default function Modal({ children, header }: ModalProps) {
+export default function Modal({ children, header, footer, onClose }: ModalProps) {
+    const overlay = useRef(null);
+
+    const handleClose = useCallback(() => {
+        if (onClose) onClose();
+    }, [onClose]);
+
+    const handleClick: MouseEventHandler = useCallback((e) => {
+        if (onClose && e.target === overlay.current) {
+            onClose();
+        }
+    }, [onClose]);
+
+    const handleKeyDown = useCallback((e: KeyboardEvent) => {
+        if (onClose && e.key === "Escape") onClose();
+    }, [onClose]);
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyDown);
+        return () => document.removeEventListener("keydown", handleKeyDown);
+    }, [handleKeyDown]);
+
     return (
-        <div className={styles.Modal}>
+        <div ref={overlay} className={styles.Modal} onClick={handleClick}>
             <div className={styles.Modal__container}>
                 <div className={styles.Modal__header}>
                     {header &&
@@ -19,11 +45,34 @@ export default function Modal({ children, header }: ModalProps) {
                             }
                         </div>
                     }
+                    <div className={styles.Modal__headerClose}>
+                        <button onClick={handleClose} className={styles.Modal__headerCloseButton}>X</button>
+                    </div>
                 </div>
                 <div className={styles.Modal__content}>
                     {children}
                 </div>
+                {footer &&
+                    <div className={styles.Modal__footer}>
+                        <div className={styles.Modal__footerContent}>
+                            {footer}
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     )
+}
+
+export function RouterModal(props: ModalProps) {
+    const router = useRouter();
+    const handleClose = useCallback(() => {
+        if (props.onClose) {
+            props.onClose();
+        }
+
+        router.back();
+    }, [router]);
+
+    return <Modal {...props} onClose={handleClose} />
 }
