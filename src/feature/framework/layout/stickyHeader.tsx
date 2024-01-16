80aaee0f8c2bbc897928d16f8661add5c13fc9b8
Jason Bert <jason.bert@scorado.com>
2024-01-16 13:04:10 +0000

    Small improvements to sticky head when scrolling; made sticky header height dynamic

--- src/feature/framework/layout/stickyHeader.tsx
+++ src/feature/framework/layout/stickyHeader.tsx
@@ -1,16 +1,21 @@
 'use client';
 
 import { PropsWithChildren } from "react";
+import { constructClasses } from "~f/web/css";
 import { useMainScrollable } from "./mainScrollable";
 import { ScrollPosition } from "./scrollable";
 import styles from './stickyHeader.module.scss';
 
 export function StickyHeader({ children }: PropsWithChildren) {
     const { hasScrolledUp, position } = useMainScrollable();
-    const scrolledClass = hasScrolledUp || position === ScrollPosition.Top ? '' : styles.Header___scrolled;
+    const cssClasses = constructClasses([
+        styles.Header,
+        position === ScrollPosition.Top ? '' : styles.Header___fixed,
+        hasScrolledUp || position === ScrollPosition.Top ? '' : styles.Header___scrolled,
+    ])
 
     return (
-        <div className={`${styles.Header} ${scrolledClass}`}>
+        <div className={cssClasses}>
             {children}
         </div>
     )
